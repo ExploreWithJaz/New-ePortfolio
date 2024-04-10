244b26e60d8484e4e026fe0f8f31184dd083d4e0
Jazper Garcia <jazpergarcia10@gmail.com>
2024-04-10 13:44:29 +0800

    modified projects

--- js/popup.js
+++ js/popup.js
@@ -17,3 +17,47 @@ closeBtn.addEventListener('click', function() {
     overlay.style.display = 'none';
   }, 300); // Same duration as CSS transition
 });
+
+const openBtnPet = document.getElementById('open-btn-pet');
+const overlayPet = document.getElementById('overlay-pet');
+const closeBtnPet = document.getElementById('close-btn-pet');
+
+openBtnPet.addEventListener('click', function() {
+  overlayPet.style.display = 'block'; 
+  setTimeout(() => {
+    overlayPet.style.opacity = '1'; 
+    document.getElementById('popup-pet').style.opacity = '1'; 
+  }, 50); // Delay added for smoother transition
+});
+
+closeBtnPet.addEventListener('click', function() {
+  overlayPet.style.opacity = '0';
+  document.getElementById('popup-pet').style.opacity = '0'; 
+  setTimeout(() => {
+    overlayPet.style.display = 'none'; 
+  }, 300); // Same duration as CSS transition
+});
+
+
+
+
+
+const openBtnMinion = document.getElementById('open-btn-minion');
+const overlayMinion = document.getElementById('overlay-minion');
+const closeBtnMinion = document.getElementById('close-btn-minion');
+
+openBtnMinion.addEventListener('click', function() {
+  overlayMinion.style.display = 'block'; 
+  setTimeout(() => {
+    overlayMinion.style.opacity = '1'; 
+    document.getElementById('popup-minion').style.opacity = '1'; 
+  }, 50); // Delay added for smoother transition
+});
+
+closeBtnMinion.addEventListener('click', function() {
+  overlayMinion.style.opacity = '0';
+  document.getElementById('popup-minion').style.opacity = '0'; 
+  setTimeout(() => {
+    overlayMinion.style.display = 'none'; 
+  }, 300); // Same duration as CSS transition
+});
